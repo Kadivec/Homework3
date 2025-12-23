2da4bb3aa3820894a194e895b9e6d9fccfef3d9e
Andraž Kadivec <137300710+Kadivec@users.noreply.github.com>
2025-12-23 21:48:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ with col_name:
     st.title("Andraž Kadivec | Web Scraping Homework 3")
 with col_logo:
     # Official EF Logo
-    logo_url = "https://github.com/Kadivec/Homework3/blob/main/EKONOMSKA-F_logoENG-HOR-CMYK_color-scaled.png"
+    logo_url = "https://github.com/Kadivec/Homework3/blob/main/logo.png"
     st.image(logo_url, width=250)
 
 st.divider()
@@ -86,6 +86,7 @@ elif page == "Reviews":
             ).properties(height=400).interactive()
 
             st.altair_chart(chart, use_container_width=True)
+
 
 
 
